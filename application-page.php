11fdbdc7f51adb2d67b4141441b3d5f3f5f6691b
GlennOnyango <glennonyango@yahoo.com>
2019-11-01 11:59:00 +0300

    New functionality added to application-page.php

--- application-page.php
+++ application-page.php
@@ -1,13 +1,32 @@
 <?php
 /*
-Template name: volunteer application for project
+Template name: volunteer application for project && Events
 */
 get_header();
 ?>
 
 <div class="application-house">
+<?php 
+if(isset($_GET['project_id']) && isset($_SESSION['email'])){
+
+require('php/Application_for_volunteer.php');
+
+$app =  new Application();
+$app->Apply($_GET['project_id'],$_SESSION['email']);
 
 
+
+}elseif(isset($_GET['event_id']) && isset($_SESSION['email'])){
+
+    $evt =  new Event();
+    $evt->confirmEvents($_GET['project_id'],$_SESSION['email']);
+    
+
+}
+
+
+
+?>
 </div>
 
 
