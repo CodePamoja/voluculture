8f8d84c628e26e33f3121840bd7ee9dee64b0b1a
GlennOnyango <glennonyango@yahoo.com>
2019-11-01 13:06:44 +0300

    with complete class events

--- php/Application_for_volunteer.php
+++ php/Application_for_volunteer.php
@@ -70,6 +70,54 @@ echo"<div>
 class Events{
     function confirmEvents($event_id,$user_email){
 
+        $user = $wpdb->get_row("SELECT * FROM  wp_user_details WHERE user_email = '$user_email'");
+        $event = $wpdb->get_row("SELECT *  FROM  wp_ngo_activity WHERE id = '$event_id'");
+
+        $confirm = $wpdb->insert('wp_event_applications',array(
+                'event_id'=> $event->id,
+                'event_name'=>$event->name,
+                'user_id'=>$user->id,
+                'user_email'=> $user->email,
+                'venue'=>$event->ngo_event_location,
+                'starttime'=>$event->ngo_event_start_time,
+                'endtime'=>$event->ngo_event_end_time 
+            ));
+
+        if($confirm) {
+
+            $subject ="VOLUCULTURE EVENT CONFIRMATION";
+            $body = "<div style='text-align:center'> 
+                        <h3> Hi <b class='color:blue'>$user->userName</b>, You have succesfully Confirmed your attandance to <b class='color:blue'>$event->ngo_event_name</b>. </h3>
+                        <p> Time: <br>
+                            $event->ngo_event_start, from $event->ngo_event_start_time to $event->ngo_event_end_time (EAT) 
+                            <br>
+                            Location: <br>
+                            $event->ngo_event_location 
+                        </p>
+                        ";
+            $headers = array('Content-Type: text/html; charset=UTF-8');
+            wp_mail($user_email,$subject,$body,$headers);
+
+            echo"<div id='display_error' style='background-color:lightblue'><h3 style='text-align:center'> 
+                    Confirmation Sent Successfully.</h3>
+                    <p>An message with the event details has been sent to your email.
+                    </div>";
+            echo"<script>
+                $(document).ready(function(){
+                    $('#display_error').fadeOut(2000);
+                });
+                </script>";
+
+        }else{
+            echo"<div id='display_error' style='background-color:lightblue'><h3 style='text-align:center'>
+                    Error Sending confirmation</h3></div>";
+            echo"<script>
+                $(document).ready(function(){
+                    $('#display_error').fadeOut(2000);
+                });
+                </script>";
+        }
+
     }
 }
 
